1f84ec936b916073c6901c3c5dfa5a7da3224794
Dalton, Cormac <Cormac.Dalton@mastercard.com>
2024-01-31 09:37:36 +0000

    encryption now works for readme example

--- encryption/encryption.go
+++ encryption/encryption.go
@@ -3,10 +3,14 @@ package encryption
 import (
 	"github.com/Jeffail/gabs/v2"
 	"github.com/mastercard/client-encryption-go/jwe"
+	"strings"
 )
 
 func EncryptPayload(payload string, config jwe.JWEConfig) string {
-	jsonPayload, _ := gabs.ParseJSON([]byte(payload))
+	jsonPayload, err := gabs.ParseJSON([]byte(payload))
+	if err != nil {
+		panic(err)
+	}
 	for jsonPathIn, jsonPathOut := range config.GetEncryptionPaths() {
 		jsonPayload = encryptPayloadPath(jsonPayload, jsonPathIn, jsonPathOut, config)
 	}
@@ -28,23 +32,48 @@ func encryptPayloadPath(jsonPayload *gabs.Container, jsonPathIn string, jsonPath
 		Kid: config.GetEncryptionKeyFingerprint(),
 		Cty: "application/json",
 	}
-
-	payload, err := jwe.Encrypt(config, jsonPayload.String(), joseHeader)
+	var payloadPath *gabs.Container
+	if jsonPathIn == "$" {
+		payloadPath = jsonPayload
+	} else {
+		if jsonPathIn[0] == '$' {
+			jsonPathIn = jsonPathIn[2:]
+		}
+		payloadPath = jsonPayload.Path(jsonPathIn)
+	}
+	payload, err := jwe.Encrypt(config, payloadPath.String(), joseHeader)
 	if err != nil {
 		panic(err)
 	}
-
 	if jsonPathIn == "$" {
 		jsonPayload = gabs.New()
 	} else {
 		jsonPayload.DeleteP(jsonPathIn)
 	}
-	jsonPayload.Set(payload, config.GetEncryptedValueFieldName())
+	if jsonPathOut == "$" {
+		jsonPayload.SetP(payload, config.GetEncryptedValueFieldName())
+	} else {
+		if jsonPathOut[0] == '$' {
+			jsonPathOut = jsonPathOut[2:]
+		}
+		jsonPayload.SetP(payload, jsonPathOut+"."+config.GetEncryptedValueFieldName())
+	}
 	return jsonPayload
 }
 
 func decryptPayloadPath(jsonPayload *gabs.Container, jsonPathIn string, jsonPathOut string, config jwe.JWEConfig) *gabs.Container {
-	inJsonObject := jsonPayload.Path(jsonPathIn).Data().(string)
+	var inJsonObject string
+	if jsonPathIn == "$" {
+		inJsonObject = jsonPayload.
+			Children()[0].
+			Data().(string)
+	} else {
+		if jsonPathIn[0] == '$' {
+			jsonPathIn = jsonPathIn[2:]
+		}
+		inJsonObject = jsonPayload.Path(jsonPathIn).
+			Data().(string)
+	}
 	jweObject, err := jwe.ParseJWEObject(inJsonObject)
 	if err != nil {
 		panic(err)
@@ -57,9 +86,17 @@ func decryptPayloadPath(jsonPayload *gabs.Container, jsonPathIn string, jsonPath
 	if jsonPathOut == "$" {
 		jsonPayload = jsonDecryptedPayload
 	} else {
-		jsonPayload.DeleteP(jsonPathIn)
-		jsonPayload.Set(jsonDecryptedPayload, jsonPathOut)
+		if jsonPathOut[0] == '$' {
+			jsonPathOut = jsonPathOut[2:]
+		}
+		jsonPayload.DeleteP(getParent(jsonPathIn))
+		jsonPayload.SetP(jsonDecryptedPayload, jsonPathOut)
 	}
-
 	return jsonPayload
+}
+
+func getParent(path string) string {
+	keys := strings.Split(path, ".")
+	parent := keys[:len(keys)-1]
+	return strings.Join(parent, ".")
 }
--- encryption/encryption_test.go
+++ encryption/encryption_test.go
@@ -37,6 +37,31 @@ func TestEncryptPayload_ShouldEncryptRootArrays(t *testing.T) {
 	assert.Equal(t, "[{},{}]", decryptedPayload)
 }
 
+func TestEncryptPayload_ShouldDecryptWholePayload(t *testing.T) {
+	const payload = `{"value1":{"value2":10,"value3":20},"value4":30}`
+
+	decryptionKeyPath := "../testdata/keys/pkcs8/test_key_pkcs8-2048.der"
+	certificatePath := "../testdata/certificates/test_certificate-2048.der"
+
+	decryptionKey, err := utils.LoadUnencryptedDecryptionKey(decryptionKeyPath)
+	assert.Nil(t, err)
+	certificate, err := utils.LoadEncryptionCertificate(certificatePath)
+	assert.Nil(t, err)
+
+	cb := jwe.NewJWEConfigBuilder()
+	jweConfig := cb.WithDecryptionKey(decryptionKey).
+		WithEncryptionPath("$", "$").
+		WithDecryptionPath("$", "$").
+		WithEncryptedValueFieldName("encryptedData").
+		WithCertificate(certificate).
+		Build()
+
+	encryptedPayload := encryption.EncryptPayload(payload, *jweConfig)
+	assert.Regexp(t, `{"encryptedData":"[^"]*?"}`, encryptedPayload)
+	decryptedPayload := encryption.DecryptPayload(encryptedPayload, *jweConfig)
+	assert.Equal(t, payload, decryptedPayload)
+}
+
 func TestDecryptPayload_ShouldEncryptRootArrays(t *testing.T) {
 	const encryptedPayload = "{" +
 		"    \"encryptedData\": \"eyJraWQiOiI3NjFiMDAzYzFlYWRlM2E1NDkwZTUwMDBkMzc4ODdiYWE1ZTZlYzBlMjI2YzA3NzA2ZTU5OTQ1MWZjMDMyYTc5IiwiY3R5IjoiYXBwbGljYXRpb24vanNvbiIsImVuYyI6IkEyNTZHQ00iLCJhbGciOiJSU0EtT0FFUC0yNTYifQ.IcTIce59pgtjODJn4PhR7oK3F-gxcd7dishTrT7T9y5VC0U5ZS_JdMoRe59_UTkJMY8Nykb2rv3Oh_jSDYRmGB_CWMIciXYMLHQptLTF5xI1ZauDPnooDMWoOCBD_d3I0wTJNcM7I658rK0ZWSByVK9YqhEo8UaIf4e6egRHQdZ2_IGKgICwmglv_uXQrYewOWFTKR1uMpya1N50MDnWax2NtnW3SljP3mARUBLBnRmOyubQCg-Mgn8fsOWWXm-KL9RrQq9AF_HJceoJl1rRgzPW7g6SLK6EjiGW_ArTmrLaOHg9bYOY_LrbyokK_M1pMo9qup70DHvjHkMZqIL3aQ.vtma3jBIo2STkquxTUX9PQ.9ZoQG0sFvQ.ms4bW3OFd03neRlex-zZ8w\"" +
@@ -59,4 +84,54 @@ func TestDecryptPayload_ShouldEncryptRootArrays(t *testing.T) {
 
 	decryptedPayload := encryption.DecryptPayload(encryptedPayload, *jweConfig)
 	assert.Equal(t, "[{},{}]", decryptedPayload)
+}
+
+func TestEncryptPayload_ShouldEncryptExampleFromREADME(t *testing.T) {
+	const payload = `{"path":{"to":{"foo":{"sensitiveField1":"sensitiveValue1","sensitiveField2":"sensitiveValue2"}}}}`
+
+	decryptionKeyPath := "../testdata/keys/pkcs8/test_key_pkcs8-2048.der"
+	certificatePath := "../testdata/certificates/test_certificate-2048.der"
+
+	decryptionKey, err := utils.LoadUnencryptedDecryptionKey(decryptionKeyPath)
+	assert.Nil(t, err)
+	certificate, err := utils.LoadEncryptionCertificate(certificatePath)
+	assert.Nil(t, err)
+
+	cb := jwe.NewJWEConfigBuilder()
+	jweConfig := cb.WithDecryptionKey(decryptionKey).
+		WithEncryptionPath("$.path.to.foo", "$.path.to.encryptedFoo").
+		WithDecryptionPath("$.path.to.encryptedFoo.encryptedData", "$.path.to.foo").
+		WithEncryptedValueFieldName("encryptedData").
+		WithCertificate(certificate).
+		Build()
+
+	encryptedPayload := encryption.EncryptPayload(payload, *jweConfig)
+	assert.Regexp(t, `{"path":{"to":{"encryptedFoo":{"encryptedData":"[^"]*?"}}}}`, encryptedPayload)
+	decryptedPayload := encryption.DecryptPayload(encryptedPayload, *jweConfig)
+	assert.Equal(t, payload, decryptedPayload)
+}
+
+func TestEncryptPayload_ShouldEncryptWithoutRootKey(t *testing.T) {
+	const payload = `{"path":{"to":{"foo":{"sensitiveField1":"sensitiveValue1","sensitiveField2":"sensitiveValue2"}}}}`
+
+	decryptionKeyPath := "../testdata/keys/pkcs8/test_key_pkcs8-2048.der"
+	certificatePath := "../testdata/certificates/test_certificate-2048.der"
+
+	decryptionKey, err := utils.LoadUnencryptedDecryptionKey(decryptionKeyPath)
+	assert.Nil(t, err)
+	certificate, err := utils.LoadEncryptionCertificate(certificatePath)
+	assert.Nil(t, err)
+
+	cb := jwe.NewJWEConfigBuilder()
+	jweConfig := cb.WithDecryptionKey(decryptionKey).
+		WithEncryptionPath("path.to.foo", "path.to.encryptedFoo").
+		WithDecryptionPath("path.to.encryptedFoo.encryptedData", "path.to.foo").
+		WithEncryptedValueFieldName("encryptedData").
+		WithCertificate(certificate).
+		Build()
+
+	encryptedPayload := encryption.EncryptPayload(payload, *jweConfig)
+	assert.Regexp(t, `{"path":{"to":{"encryptedFoo":{"encryptedData":"[^"]*?"}}}}`, encryptedPayload)
+	decryptedPayload := encryption.DecryptPayload(encryptedPayload, *jweConfig)
+	assert.Equal(t, payload, decryptedPayload)
 }
